de344b90acd12bd5bc6733ef3e9750aef97420bc
Zeyad Alamoudi <zyadalamoudi@hotmail.com>
2023-10-30 21:49:10 +0300

    result prop

---
+++ src/ShortenedURL.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ShortenedURL(props) {
+  return (
+    <div className='result'>
+      <p id='shortURL'>{props.shortURL}</p>
+    </div>
+  );
+}
+export default ShortenedURL;
