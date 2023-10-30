bf0545eac1d53aad02792d29242604fd108f7df5
Zeyad Alamoudi <zyadalamoudi@hotmail.com>
2023-10-30 21:17:49 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [longURL, setLongURL] = useState('');
+  const [shortURL, setShortURL] = useState('');
+  function handleClick(){
+    const randomID = Math.random().toString(36).substring(7)
+    setShortURL('http://cpit405.co/' + randomID);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tiny URL</h1>
+      <div className='form'>
+        <label htmlFor='longURL'>Enter a long URL</label>
+        <input type='text' id='longURL' value={longURL} onChange={(e) => setLongURL(e.target.value)}/>
+      </div>
+      <button className='btn' onClick={handleClick}>shorten URL</button>
+      <div className='result'>
+        <p id='shortURL'>{shortURL}</p>
+      </div>
     </div>
   );
 }
